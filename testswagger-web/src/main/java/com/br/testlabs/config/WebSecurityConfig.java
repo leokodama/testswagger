dad37947c97a613a4d289eb23e7fe26c91b21570
leokodama <leonardo.kodama@oi.net.br>
2017-06-12 14:34:00 -0300

    config Procfile

---
+++ testswagger-web/src/main/java/com/br/testlabs/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.br.testlabs.config;
+
+import static org.springframework.http.HttpMethod.POST;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.br.testlabs.security.JWTAuthenticationFilter;
+import com.br.testlabs.security.JWTLoginFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/").permitAll().antMatchers(POST, "/login").permitAll()
+                .anyRequest().authenticated().and()
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
+        
+    }
+}
